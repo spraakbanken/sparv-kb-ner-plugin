6c2e5aa35e2248911540c372ff26379edc15078d
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-02-28 14:40:42 +0100

    fix: align index and tokens
    fixes #2

--- sparv_kb_ner/core.py
+++ sparv_kb_ner/core.py
@@ -45,12 +45,16 @@ def parse_kb_ner_output(sentence: Annotation, word: Annotation, out_ne: Output):
     ]
     # stdout = run_nlp(stdin)
     out_annotation = word.create_empty_attribute()
-    for sent, sent_to_tag in zip(sentences, sentences_to_tag):
+    for sent, sent_to_tag in zip(sentences, sentences_to_tag, strict=True):
         tagged_tokens = run_nlp_on_sentence(sent_to_tag)
         logger.debug("tagged_tokens = %s", tagged_tokens)
-        for token_index, tagged_token in zip(sent, tagged_tokens):
+        logger.debug("sent = %s", sent)
+        for token_index, tagged_token in zip(sent, tagged_tokens, strict=True):
             logger.debug(
-                "token_index = %d, tagged_token = %s", token_index, tagged_token
+                "token_index = %d, tagged_token = %s, token_work = %s",
+                token_index,
+                tagged_token,
+                token_word[token_index],
             )
             # tag = tagged_token.strip().split(TAG_SEP)[TAG_COLUMN]
             # tag = tag_mapping.get(tag, tag)
@@ -72,36 +76,44 @@ class Token:
 def run_nlp_on_sentence(sentence: str) -> list[str]:
     tokens = []
     for token in nlp(sentence):
+        logger.debug("nlp: token = %s", token)
         if token["word"].startswith("##"):
+            logger.debug("found ## in %s", token["word"])
             tokens[-1].word += token["word"][2:]
             tokens[-1].end = token["end"]
         else:
             tokens.append(Token(**token))
+        logger.debug("tokens[-1].word = %s", tokens[-1].word)
+        logger.debug("tokens = %s", tokens)
     return interleave_tags_and_sentence(tokens, sentence)
 
 
 def interleave_tags_and_sentence(tokens: list[Token], sentence: str) -> list[str]:
+    logger.debug("tokens = %s", tokens)
+    logger.debug("sentence = %s", sentence)
     tags = []
     end = len(sentence)
     curr: int = 0
     curr_token = 0
     while curr < end:
+        print(f"while {curr=} < {end=}: {curr_token=}, {sentence[curr:]=}")
         # print(f"{curr=}, {end=}, {curr_token=}")
         if curr_token < len(tokens) and tokens[curr_token].start == curr:
+            print("in token")
             logger.debug(
                 "word = %s, tag = %s",
                 tokens[curr_token].word,
                 tokens[curr_token].entity,
             )
             tags.append(tokens[curr_token].entity)
-            curr = tokens[curr_token].end
+            curr = find_word_ending(sentence, tokens[curr_token].end)
             curr_token += 1
         else:
+            print("not in token")
             part_end = tokens[curr_token].start if curr_token < len(tokens) else end
             part = sentence[curr:part_end]
             logger.debug("part='%s'", part)
-            parts = part.strip()
-            if parts:
+            if parts := part.strip():
                 parts = parts.split(" ")
                 logger.debug("parts=%s", parts)
                 # print(f"{parts=}")
@@ -110,3 +122,10 @@ def interleave_tags_and_sentence(tokens: list[Token], sentence: str) -> list[str
                     tags.append("")
             curr = part_end
     return tags
+
+
+def find_word_ending(sentence: str, token_end: int) -> int:
+    """Find first whitespace from token_end or above."""
+    while token_end < len(sentence) and sentence[token_end] != " ":
+        token_end += 1
+    return token_end
---
+++ tests/test_sparv_kbner.py
@@ -0,0 +1,173 @@
+from sparv_kb_ner.core import Token, find_word_ending, interleave_tags_and_sentence
+import pytest
+
+
+SENTENCES = [
+    "Ikea ( namnet är bildat av initialerna för Ingvar Kamprad Elmtaryd Agunnaryd ) är ett multinationellt möbelföretag som grundades 1943 av Ingvar Kamprad .",  # noqa: E501
+    "Under verksamhetsåret 2012 omsatte Ikeakoncernen 241 miljarder kronor .",
+]
+
+
+@pytest.mark.parametrize(
+    "tokens, sentence_index, expected",
+    [
+        (
+            [
+                Token(
+                    word="Ikea", entity="ORG", start=0, end=4, score=0.9993444, index=1
+                ),
+                Token(
+                    word="Ingvar",
+                    entity="PER",
+                    start=43,
+                    end=49,
+                    score=0.9993979,
+                    index=10,
+                ),
+                Token(
+                    word="Kamprad",
+                    entity="PER",
+                    start=50,
+                    end=57,
+                    score=0.99831116,
+                    index=11,
+                ),
+                Token(
+                    word="Elmtaryd",
+                    entity="PER",
+                    start=58,
+                    end=66,
+                    score=0.7953129,
+                    index=12,
+                ),
+                Token(
+                    word="Agunnaryd",
+                    entity="LOC",
+                    start=67,
+                    end=76,
+                    score=0.9898454,
+                    index=15,
+                ),
+                Token(
+                    word="1943",
+                    entity="TME",
+                    start=129,
+                    end=133,
+                    score=0.9958897,
+                    index=28,
+                ),
+                Token(
+                    word="Ingvar",
+                    entity="PER",
+                    start=137,
+                    end=143,
+                    score=0.99676883,
+                    index=30,
+                ),
+                Token(
+                    word="Kamprad",
+                    entity="PER",
+                    start=144,
+                    end=151,
+                    score=0.9973974,
+                    index=31,
+                ),
+            ],
+            0,
+            [
+                "ORG",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "PER",
+                "PER",
+                "PER",
+                "LOC",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "",
+                "TME",
+                "",
+                "PER",
+                "PER",
+                "",
+            ],
+        ),
+        (
+            [
+                Token(
+                    word="verksamhetsåret",
+                    entity="TME",
+                    start=6,
+                    end=21,
+                    score=0.9601936,
+                    index=2,
+                ),
+                Token(
+                    word="2012",
+                    entity="TME",
+                    start=22,
+                    end=26,
+                    score=0.99914443,
+                    index=4,
+                ),
+                Token(
+                    word="Ikea",
+                    entity="ORG",
+                    start=35,
+                    end=39,
+                    score=0.99542964,
+                    index=6,
+                ),
+                Token(
+                    word="241",
+                    entity="MSR",
+                    start=49,
+                    end=52,
+                    score=0.99984705,
+                    index=8,
+                ),
+                Token(
+                    word="miljarder",
+                    entity="MSR",
+                    start=53,
+                    end=62,
+                    score=0.9998272,
+                    index=9,
+                ),
+                Token(
+                    word="kronor",
+                    entity="MSR",
+                    start=63,
+                    end=69,
+                    score=0.99974865,
+                    index=10,
+                ),
+            ],
+            1,
+            ["", "TME", "TME", "", "ORG", "MSR", "MSR", "MSR", ""],
+        ),
+    ],
+)
+def test_interleave_tags_and_sentence(
+    tokens: list[Token], sentence_index: int, expected: list[str]
+):
+    assert interleave_tags_and_sentence(tokens, SENTENCES[sentence_index]) == expected
+
+
+@pytest.mark.parametrize(
+    "sentence_index, token_end, expected", [(1, 21, 21), (1, 39, 48)]
+)
+def test_find_word_ending(sentence_index: int, token_end: int, expected: int) -> None:
+    sentence = SENTENCES[sentence_index]
+
+    assert find_word_ending(sentence, token_end) == expected
+    assert sentence[find_word_ending(sentence, token_end)] == " "
