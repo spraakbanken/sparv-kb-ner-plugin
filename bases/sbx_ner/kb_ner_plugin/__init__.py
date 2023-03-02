928f0f08a986fe0728c515b25423680b5192091e
Kristoffer Andersson <kristoffer.andersson@gu.se>
2023-03-02 11:30:09 +0100

    build: remove polylith structure
    Currently not needed, and SPARV doesn't work well with namespace.

--- bases/sbx_ner/kb_ner_plugin/__init__.py
+++
@@ -1,5 +0,0 @@
-from sbx_ner.kb_ner_plugin import core
-
-__all__ = ["core"]
-
-__description__ = "Using KB NER"
--- test/bases/sbx_ner/kb_ner_plugin/test_core.py
+++
@@ -1,5 +0,0 @@
-from sbx_ner.kb_ner_plugin import core
-
-
-def test_sample():
-    assert core is not None
